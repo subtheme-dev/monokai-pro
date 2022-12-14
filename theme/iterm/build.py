9fc283da92f49f3219984d104d252759386d60bc
Jonathan Esposito <jonathan@esposito.page>
2022-12-14 17:41:51 -0500

    Added Terminal theme

--- theme/iterm/build.py
+++ theme/iterm/build.py
@@ -1,19 +1,20 @@
 import sublate as sub
 
-print("[+] Terminal")
+print("[+] iTerm")
 
 for theme in sub.data["colors"]:
     # convert colors to rgb decimal
+    _theme = {"colors": {}}
     for k,v in theme["colors"].items():
         r, g, b = tuple(int(v[1:][i:i+2], 16)/255 for i in (0, 2, 4))
-        theme["colors"][k] = {
+        _theme["colors"][k] = {
             "r":  r,
             "g":  g,
             "b":  b,
         }
 
     sub.render(f"{theme['name']}.itermcolors".replace(" ", "_"), "template.itermcolors", {
-        "theme": theme
+        "theme": _theme
     })
 
 sub.rm("template.itermcolors")
---
+++ theme/terminal/build.py
@@ -0,0 +1,9 @@
+import sublate as sub
+
+print("[+] Terminal")
+
+for theme in sub.data["colors"]:
+    # TODO: render with template
+    pass
+
+sub.rm("build.py")
